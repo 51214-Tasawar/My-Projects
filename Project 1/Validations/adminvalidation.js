87e50e75cab827dafee014b96ba1344536025c97
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-03 21:38:37 +0500

    Code Update

--- Project 1/Validations/adminvalidation.js
+++ Project 1/Validations/adminvalidation.js
@@ -1,5 +1,10 @@
 const adminValidate = require("joi");
 
 const  CreateValidAdmin = new adminValidate.object({
-    
+
+    name : adminValidate.string().min(6).max(40).required() ,
+    username : adminValidate.string().min(6).max(30).required(),
+    email : adminValidate.string().min(8).max(50),
+    password: adminValidate.string().min(8).max(1000)
 })
+
