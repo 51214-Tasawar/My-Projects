011d6fd21864888dd7514943c4b6837f8c2e7def
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-04 00:31:02 +0500

    Validation Scheme Completed

---
+++ Project 1/Validations/customervalidation.js
@@ -0,0 +1,54 @@
+const joi = require("joi") ;
+const Create = joi.object({
+    name : joi.string().min(6).max(20).required(),
+    username : joi.string().min(8).max(50).required(),
+    email : joi.string().min(8).max(50).required() ,
+    password : joi.string().min(8).max(1000)
+})
+const Update = joi.object({
+    name : joi.string().min(6).max(20),
+    username : joi.string().min(8).max(50).required(),
+    email : joi.string().min(8).max(50),
+    password : joi.string().min(8).max(1000)
+})
+const getdelete = joi.object({
+    username : joi.string().min(8).max(50).required
+})
+
+module.exports ={
+    Createcustomer :async( req, res , next )=>{
+     try {
+        await Create.validateAsync(req.body)
+        next()
+     }catch(error){
+        res.send({
+            code : 400 ,
+            error : error.message
+        })
+     }
+    } ,
+
+    Updatecustomer:async(req , res , next )=>{
+        try {
+            await Update.validateAsync(req.body)
+            next()
+         }catch(error){
+            res.send({
+                code : 400 ,
+                error : error.message
+            })
+         }
+    } ,
+   getdeleteCustomer:async(req , res , next )=>{
+      
+    try {
+        await getdelete.validateAsync(req.query)
+        next()
+     }catch(error){
+        res.send({
+            code : 400 ,
+            error : error.message
+        })
+     }
+   }
+} 
--- Project 1/routes/customerroute.js
+++ Project 1/routes/customerroute.js
@@ -4,13 +4,20 @@ const {createCustomer ,
     deleteCustomer
 } = require("../controller/customercontroller")
 
+const {
+    Createcustomer ,
+    Updatecustomer ,
+    getdeleteCustomer 
+
+} = require("../Validations/customervalidation")
+
 const customerroute = require("express").Router() ;
 
 
-customerroute.get("/get_customer" , getCustomer ) ;
-customerroute.post("/create_customer" , createCustomer) ;
-customerroute.patch("/update_customer" , updateCustomer) ;
-customerroute.delete("/delete_customer" , deleteCustomer) ;
+customerroute.get("/get_customer", getdeleteCustomer , getCustomer ) ;
+customerroute.post("/create_customer" , Createcustomer , createCustomer) ;
+customerroute.patch("/update_customer" , Updatecustomer , updateCustomer) ;
+customerroute.delete("/delete_customer" , getdeleteCustomer , deleteCustomer) ;
 
 module.exports = customerroute ;
 
