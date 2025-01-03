74d94ae93bc0f158704feb9a83dc7942a267ed39
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-03 23:50:18 +0500

    New Code

--- Project 1/Validations/adminvalidation.js
+++ Project 1/Validations/adminvalidation.js
@@ -1,11 +1,23 @@
 const adminValidate = require("joi");
+const { get } = require("../routes/adminroute");
 
 const  CreateValidAdmin = adminValidate.object({
 
     name : adminValidate.string().min(6).max(40).required() ,
     username : adminValidate.string().min(6).max(30).required(),
+    email : adminValidate.string().min(8).max(50).required(),
+    password: adminValidate.string().min(8).max(1000)
+})
+
+const UpdateValidAdmin = adminValidate.object({
+    name : adminValidate.string().min(6).max(40),
+    username : adminValidate.string().min(6).max(30).required(),
     email : adminValidate.string().min(8).max(50),
     password: adminValidate.string().min(8).max(1000)
+})
+
+const getdeleteValidAdmin = adminValidate.object({
+    username : adminValidate.string().min(6).max(30).required()
 })
 
 module.exports = {
@@ -15,7 +27,30 @@ module.exports = {
        next()
       }catch(error){
         res.send({
-            code : 200 ,
+            code : 400 ,
+            error : error.message
+        })
+      }
+    } ,
+
+    UpdateAdmin :async( req , res , next )=>{
+            try{
+           await UpdateValidAdmin.validateAsync(req.body) ;
+           next()
+            }catch(error){
+             res.send({
+                code : 400 ,
+                error : error.message
+             })
+            }
+    } ,
+    getdeleteAdmin :async(req , res , next)=>{
+      try {
+      await getdeleteValidAdmin.validateAsync(req.query)
+       next()
+      }catch(error){
+        res.send({
+            code : 400 ,
             error : error.message
         })
       }
--- Project 1/routes/adminroute.js
+++ Project 1/routes/adminroute.js
@@ -1,12 +1,12 @@
 const {createAdmin ,getAdmin , updateAdmin , deleteAdmin} = require("../controller/admincontroller")
-const {CreateAdmin} = require("../Validations/adminvalidation")
+const {CreateAdmin , UpdateAdmin , getdeleteAdmin} = require("../Validations/adminvalidation")
 
 const  adminroute  = require("express").Router();
 
-adminroute.get("/get_admin" , getAdmin);
+adminroute.get("/get_admin" , getdeleteAdmin,getAdmin);
 adminroute.post("/create_admin" ,CreateAdmin,  createAdmin);
-adminroute.patch("/update_admin" , updateAdmin);
-adminroute.delete("/delete_admin" ,deleteAdmin);
+adminroute.patch("/update_admin" , UpdateAdmin , updateAdmin);
+adminroute.delete("/delete_admin",getdeleteAdmin,deleteAdmin);
 
 module.exports = adminroute
 
