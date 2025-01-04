80a5d63a8b2138489d78c6d0ed7b0554c7b19503
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-04 23:04:30 +0500

    Admin Table Created

---
+++ Project 1/models/Tables/adminTable.js
@@ -0,0 +1,38 @@
+const {DataTypes , Model, Sequelize} = require("sequelize") ;
+
+class adminTable extends Model { } ;
+
+adminTable.init (
+    {
+        adminId : {
+            primaryKey : true ,
+            type : DataTypes.STRING(1000)
+        } ,
+        name :{
+            type : DataTypes.STRING(100) ,
+            allowNull : false
+        } ,
+        username : {
+            type : DataTypes.STRING(100) ,
+            allowNull : false ,
+            unique : true
+        } ,
+        email :{ 
+            type : DataTypes.STRING(100) ,
+            allowNull : false
+        } ,
+        password : {
+            type : DataTypes.STRING(100) ,
+            allowNull : false ,
+        }
+    } ,
+    {
+    
+        name : "adminTable" ,
+        timestamps : true  ,
+        paranoid : true
+
+    }
+)
+
+module.exports = adminTable ;
