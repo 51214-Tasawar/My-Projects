7d923b13c2b5f00a0b230bd12966c618371c9f8a
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-04 23:27:42 +0500

    Vendor Table Created

--- Project 1/models/Tables/vendorTable.js
+++ Project 1/models/Tables/vendorTable.js
@@ -7,9 +7,31 @@ vendorTable.init (
         vendorId:{
             primaryKey : true ,
             type : DataTypes.STRING(1000)
+        } ,
+        name :{
+            type : DataTypes.STRING(100) ,
+            allowNull : false
+        } ,
+        username: {
+            type : DataTypes.STRING(100) ,
+            allowNull : false ,
+            unique : true 
+        } ,
+        email:{
+           type : DataTypes.STRING(100) ,
+           allowNull : false
+        } ,
+        password:{
+           type : DataTypes.STRING(100) ,
+           allowNull : false
         }
     } ,
     {
+        name : "vendorTable" ,
+        timestamps : true ,
+        paranoid : true 
 
     }
 )
+
+module.exports = vendorTable ;
