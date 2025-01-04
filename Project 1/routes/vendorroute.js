07891d5558515e34dca1813a15a81242bd06b1ef
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-04 22:36:16 +0500

    Code Update

--- Project 1/routes/vendorroute.js
+++ Project 1/routes/vendorroute.js
@@ -14,11 +14,10 @@ const {
 const vendorroute  = require("express").Router();
 
 vendorroute.get("/get_vendor" , getdeletevendor,getvendor)
-vendorroute.post("/create_vendor" , createvendor)
+vendorroute.post("/create_vendor" ,  newVendor ,createvendor)
 vendorroute.patch("/update_vendor",updatenewVendor , updatevendor)
 vendorroute.delete("/delete_vendor" , getdeletevendor,deletevendor)
 
  module.exports = vendorroute
 
-//  newVendor ,
 
