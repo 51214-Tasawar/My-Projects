718849b5131874b186a72fa92487f6e6e6ad4c68
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-01-10 23:54:49 +0500

    New Code

--- Project 1/models/Tables/productVariation.js
+++ Project 1/models/Tables/productVariation.js
@@ -7,7 +7,22 @@ ProductVariation.init(
     VariationId :{
        primaryKey : true ,
        type : DataTypes.STRING(1000)
+    } ,
+    variationName :{
+        type : DataTypes.STRING(),
+        allowNull:false ,
+        unique : true
+    } ,
+    descryption: {
+        type : DataTypes.STRING(1000) ,
+        allowNull : false 
+    } ,
+    quantity : {
+        type : DataTypes.STRING(1000) ,
+        allowNull : false 
     }
    } ,
   {}
 )
+
+module.exports = ProductVariation ;
